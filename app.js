f5d1f23d903b54729874da89d29aa5e023e59de1
DustVoyager <ohjamdol@gmail.com>
2024-09-20 22:37:17 +0900

    basic middleware

--- app.js
+++ app.js
@@ -5,14 +5,33 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-// app.get("/", (req, res) => {
-//   res.send("hello express");
-// });
-
-// file은 sendFile
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.use((req, res, next) => {
+  console.log("모든 요청에 실행하고 싶습니다.");
+  next();
 });
+
+app.get(
+  "/",
+  (req, res, next) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+    next("route");
+  },
+  (req, res) => {
+    console.log("실행되나요");
+  }
+);
+
+app.get("/", (req, res) => {
+  console.log("실행됩니다.");
+});
+
+app.get("/category/javascript", (req, res) => {
+  res.send("hello javascript");
+});
+
+// app.get("/category/:name", (req, res) => {
+//   res.send("hello wildcard");
+// });
 
 app.post("/", (req, res) => {
   res.send("hello express");
@@ -20,6 +39,16 @@ app.post("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("hello about");
+});
+
+app.use((req, res, next) => {
+  res.send("404 입니다.");
+});
+
+//error middleware는 반드시 4개를 다 작성해줘야한다.
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send("에러가 발생하였습니다.");
 });
 
 app.listen(app.get("port"), () => {
